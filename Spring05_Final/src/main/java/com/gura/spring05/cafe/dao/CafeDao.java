7e480a110fae1f83fa5059acb153fb2bdccc331b
jjugit config --global user.name jjy321501git config --global user.email jjyu. <jjy321501@naver.com>
2021-02-04 11:34:58 +0900

    Spring05 gallery 리스트,업로드폼 구현 (사진 업로드 기능 구현중)

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeDao.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/dao/CafeDao.java
@@ -8,11 +8,9 @@ import com.gura.spring05.cafe.dto.CafeDto;
 public interface CafeDao {
 	//글 추가
 	public void insert(CafeDto dto);
-	//글 수정
-	public void update(CafeDto dto);
-	//글 삭제
+	//파일 삭제
 	public void delete(int num);
-	//글 하나의 정보 얻어오기
+	//파일 하나의 정보 얻어오기
 	public CafeDto getData(int num);
 	//글 목록 얻어오기 (페이징 처리와 검색 키워드를 고려한 목록)
 	public List<CafeDto> getList(CafeDto dto);
@@ -20,6 +18,8 @@ public interface CafeDao {
 	public int getCount(CafeDto dto);
 	//글 조회수 올리기
 	public void addViewCount(int num);
+	
+	public void update(CafeDto dto);
 }
 
 
