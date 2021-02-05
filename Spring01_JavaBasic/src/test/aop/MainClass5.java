1d744b795d15db094ed84e378354a1999e5e88f1
jjugit config --global user.name jjy321501git config --global user.email jjyu. <jjy321501@naver.com>
2021-02-05 15:44:57 +0900

    Spring01 @around 예제 추가함

--- Spring01_JavaBasic/src/test/aop/MainClass5.java
+++ Spring01_JavaBasic/src/test/aop/MainClass5.java
@@ -1,26 +1,25 @@
 package test.aop;
 
-import java.applet.AppletContext;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import test.util.WritingUtil;
+import test.cafe.Americano;
+import test.cafe.Latte;
+import test.cafe.Milk;
+import test.cafe.StarBucks;
 
-public class MainClass3 {
+public class MainClass5 {
 	
 	public static void main(String[] args) {
 		//init.xml 문서를 로딩한다 (spring bean container 를 만든다)
 		ApplicationContext context=
 				new ClassPathXmlApplicationContext("/test/aop/init.xml");
 		//spring bean container 에서 WritingUtil type 의 참조값 얻어오기
-		WritingUtil util=context.getBean(WritingUtil.class);
+		StarBucks star=context.getBean(StarBucks.class);
+		star.orderOne(new Americano());
+		star.orderTwo(new Latte(), new Americano());
+		star.orderThree(new Latte(), new Milk(), new Americano());
 		
-		util.sendGreet("안녕하세요");
-		util.sendGreet("좋은아침");
-		util.sendGreet("안녕바보");
-		util.sendGreet("똥개야");
-
 		System.out.println("main 메소드가 종료됩니다");
 		
 	}
